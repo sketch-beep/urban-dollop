5aa8089dd92c67a313c571a82f88ac0c94f47de6
Ganeshaâ <ganesha4281@gmail.com>
2025-09-25 10:46:17 -0400

    message

---
+++ calculateArea/cA.js
@@ -0,0 +1,11 @@
+let length;
+let width;
+
+function cA(){
+    length=parseFloat(document.getElementById('length').value);
+    width=paarseFloat(document.getElementById('width').value);
+
+    let area=length*width;
+
+    document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
+}
